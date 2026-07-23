9e7b7c8fe96f779d62c2bd9bc87fa632eace40c4
ByteHunter13 <jorgemoglz95@gmail.com>
2026-07-22 19:43:39 -0600

    Uso de SELECT para explorar registros de tablas

---
+++ 01. SELECT.sql
@@ -0,0 +1,11 @@
+-- Obtención de los registros de la tabla
+SELECT * FROM employee_demographics;
+
+SELECT * FROM employee_salary;
+
+SELECT * FROM parks_departments;
+
+-- Selección de una tabla aplicando una operación
+-- Operaciones PEMDAS: regla nemotécnica que indica la jerarquía estándar para resolver operaciones matemáticas combinadas
+SELECT age, age+10
+FROM employee_demographics;
